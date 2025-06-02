e1849e9eecb8728e319187ac380828421780d7e5
YuAnLee1 <yuanlee2004@gmail.com>
2025-06-02 18:24:47 -0500

    Filtros funcionales
    Los filtros ya funcionan

--- QhatuPUCPWeb/QhatuPUCPPresentacion/Inicio/PaginaInicio.aspx.cs
+++ QhatuPUCPWeb/QhatuPUCPPresentacion/Inicio/PaginaInicio.aspx.cs
@@ -20,32 +20,32 @@ namespace QhatuPUCPPresentacion.Inicio
 
         private void CargarFiltros()
         {
-            // CURSOS
+            // cargar cursos
             CursoWSClient cursoClient = new CursoWSClient();
             curso[] cursos = cursoClient.listarCurso();
             ddlCurso.Items.Clear();
             ddlCurso.Items.Add(new ListItem("Curso", "0"));
-            foreach (var c in cursos)
+            foreach (curso c in cursos)
             {
                 ddlCurso.Items.Add(new ListItem(c.nombre, c.idCurso.ToString()));
             }
 
-            // FACULTADES
+            // cargar facultades
             FacultadWSClient facultadClient = new FacultadWSClient();
             facultad[] facultades = facultadClient.listarFacultad();
             ddlFacultad.Items.Clear();
             ddlFacultad.Items.Add(new ListItem("Facultad", "0"));
-            foreach (var f in facultades)
+            foreach (facultad f in facultades)
             {
                 ddlFacultad.Items.Add(new ListItem(f.nombre, f.idFacultad.ToString()));
             }
 
-            // ESPECIALIDADES
+            // cargar especialidades
             EspecialidadWSClient especialidadClient = new EspecialidadWSClient();
             especialidad[] especialidades = especialidadClient.listarEspecialidad();
             ddlEspecialidad.Items.Clear();
             ddlEspecialidad.Items.Add(new ListItem("Especialidad", "0"));
-            foreach (var e in especialidades)
+            foreach (especialidad e in especialidades)
             {
                 ddlEspecialidad.Items.Add(new ListItem(e.nombre, e.idEspecialidad.ToString()));
             }
@@ -56,26 +56,35 @@ namespace QhatuPUCPPresentacion.Inicio
             try
             {
                 PublicacionWSClient client = new PublicacionWSClient();
-                publicacion[] publicacionesWS = client.listarPublicacion();
+                List<publicacion> publicaciones = new List<publicacion>();
 
-                List<publicacion> publicaciones = publicacionesWS.ToList();
-
-                //// Aplicar filtros
-                //if (ddlFacultad.SelectedIndex > 0)
-                //    publicaciones = publicaciones.Where(p => p.publicacionesFacultades== ddlFacultad.SelectedItem.Text).ToList();
-
-                //if (ddlEspecialidad.SelectedIndex > 0)
-                //    publicaciones = publicaciones.Where(p => p.publicacionesEspecialidades == ddlEspecialidad.SelectedItem.Text).ToList();
-
-                //if (ddlCurso.SelectedIndex > 0)
-                //    publicaciones = publicaciones.Where(p => p.publicacionesCursos == ddlCurso.SelectedItem.Text).ToList();
+                // Lógica de filtros: prioridad Facultad > Especialidad > Curso
+                if (ddlFacultad.SelectedIndex > 0)
+                {
+                    int idFacultad = int.Parse(ddlFacultad.SelectedValue);
+                    publicaciones = client.listarPorFacultad(idFacultad).ToList();
+                }
+                else if (ddlEspecialidad.SelectedIndex > 0)
+                {
+                    int idEspecialidad = int.Parse(ddlEspecialidad.SelectedValue);
+                    publicaciones = client.listarPorEspecialidad(idEspecialidad).ToList();
+                }
+                else if (ddlCurso.SelectedIndex > 0)
+                {
+                    int idCurso = int.Parse(ddlCurso.SelectedValue);
+                    publicaciones = client.listarPorCurso(idCurso).ToList();
+                }
+                else
+                {
+                    // Si no hay filtro, cargar todas
+                    publicaciones = client.listarPublicacion().ToList();
+                }
 
                 rptPublicaciones.DataSource = publicaciones;
                 rptPublicaciones.DataBind();
             }
             catch (Exception ex)
             {
-                // Puedes mostrar un mensaje de error o loguearlo
                 Console.WriteLine("Error al cargar publicaciones: " + ex.Message);
             }
         }
