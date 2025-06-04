7c21206e574670ba9062bcbdb07ba8f15872ef87
jhntan2075 <jhonatandavila2705@outlook.com>
2025-06-04 06:51:14 -0500

    Favoritos
    Intento de Favoritos.aspx

--- QhatuPUCPWeb/QhatuPUCPPresentacion/Inicio/PaginaInicio.aspx
+++ QhatuPUCPWeb/QhatuPUCPPresentacion/Inicio/PaginaInicio.aspx
@@ -1,5 +1,54 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MainLayout.Master" AutoEventWireup="true" CodeBehind="PaginaInicio.aspx.cs" Inherits="QhatuPUCPPresentacion.Inicio.PaginaInicio" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="Contenido" runat="server">
+    <!-- Filtros -->
+    <div class="container mb-4">
+        <div class="row g-2">
+            <div class="col-auto">
+                <asp:DropDownList ID="ddlFacultad" runat="server" CssClass="form-select" AutoPostBack="true" OnSelectedIndexChanged="ddlFacultad_SelectedIndexChanged" />
+            </div>
+            <div class="col-auto">
+                <asp:DropDownList ID="ddlEspecialidad" runat="server" CssClass="form-select" AutoPostBack="true" OnSelectedIndexChanged="ddlEspecialidad_SelectedIndexChanged" />
+            </div>
+            <div class="col-auto">
+                <asp:DropDownList ID="ddlCurso" runat="server" CssClass="form-select" AutoPostBack="true" OnSelectedIndexChanged="ddlCurso_SelectedIndexChanged" />
+            </div>
+        </div>
+    </div>
+
+    <!-- Cards con contenedor -->
+    <div class="container">
+        <div class="cards-box p-4">
+            <div class="row g-4">
+                <asp:Repeater ID="rptPublicaciones" runat="server">
+                    <ItemTemplate>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="card h-100 shadow-sm border-0">
+                                <img src='<%# Eval("rutaImagen ") %>' class="card-img-top" style="height: 180px; object-fit: cover;" />
+                                <div class="card-body d-flex justify-content-between align-items-center">
+                                    <p class="card-text mb-0"><%# Eval("titulo") %></p>
+                                </div>
+                            </div>
+                        </div>
+                    </ItemTemplate>
+                </asp:Repeater>
+            </div>
+        </div>
+    </div>
+
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="Scripts" runat="server">
+    
+</asp:Content>
+
+
+<!--
+<%@ Page Title="" Language="C#" MasterPageFile="~/MainLayout.Master" AutoEventWireup="true" CodeBehind="PaginaInicio.aspx.cs" Inherits="QhatuPUCPPresentacion.Inicio.PaginaInicio" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
     <style>
         .pagina-inicio-small {
             font-size: 0.9rem;
@@ -45,7 +94,6 @@
 
 <asp:Content ID="Content2" ContentPlaceHolderID="Contenido" runat="server">
     <div class="pagina-inicio-small">
-        <!-- Filtros -->
         <div class="container mb-4">
             <div class="row g-2">
                 <div class="col-auto">
@@ -63,7 +111,6 @@
             </div>
         </div>
 
-        <!-- Publicaciones -->
         <div class="container">
             <div class="cards-box p-4">
                 <div class="row g-4">
@@ -107,5 +154,6 @@
 </asp:Content>
 
 <asp:Content ID="Content3" ContentPlaceHolderID="Scripts" runat="server">
-    <!-- Puedes incluir scripts si lo necesitas -->
+    
 </asp:Content>
+-->
--- QhatuPUCPWeb/QhatuPUCPPresentacion/Inicio/PaginaInicio.aspx.cs
+++ QhatuPUCPWeb/QhatuPUCPPresentacion/Inicio/PaginaInicio.aspx.cs
@@ -9,6 +9,106 @@ namespace QhatuPUCPPresentacion.Inicio
 {
     public partial class PaginaInicio : System.Web.UI.Page
     {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarFiltros();
+                CargarPublicaciones();
+            }
+        }
+
+        private void CargarFiltros()
+        {
+            // cargar cursos
+            CursoWSClient cursoClient = new CursoWSClient();
+            curso[] cursos = cursoClient.listarCurso();
+            ddlCurso.Items.Clear();
+            ddlCurso.Items.Add(new ListItem("Curso", "0"));
+            foreach (curso c in cursos)
+            {
+                ddlCurso.Items.Add(new ListItem(c.nombre, c.idCurso.ToString()));
+            }
+
+            // cargar facultades
+            FacultadWSClient facultadClient = new FacultadWSClient();
+            facultad[] facultades = facultadClient.listarFacultad();
+            ddlFacultad.Items.Clear();
+            ddlFacultad.Items.Add(new ListItem("Facultad", "0"));
+            foreach (facultad f in facultades)
+            {
+                ddlFacultad.Items.Add(new ListItem(f.nombre, f.idFacultad.ToString()));
+            }
+
+            // cargar especialidades
+            EspecialidadWSClient especialidadClient = new EspecialidadWSClient();
+            especialidad[] especialidades = especialidadClient.listarEspecialidad();
+            ddlEspecialidad.Items.Clear();
+            ddlEspecialidad.Items.Add(new ListItem("Especialidad", "0"));
+            foreach (especialidad e in especialidades)
+            {
+                ddlEspecialidad.Items.Add(new ListItem(e.nombre, e.idEspecialidad.ToString()));
+            }
+        }
+
+        private void CargarPublicaciones()
+        {
+            try
+            {
+                PublicacionWSClient client = new PublicacionWSClient();
+                List<publicacion> publicaciones = new List<publicacion>();
+
+                // Lógica de filtros: prioridad Facultad > Especialidad > Curso
+                if (ddlFacultad.SelectedIndex > 0)
+                {
+                    int idFacultad = int.Parse(ddlFacultad.SelectedValue);
+                    publicaciones = client.listarPorFacultad(idFacultad).ToList();
+                }
+                else if (ddlEspecialidad.SelectedIndex > 0)
+                {
+                    int idEspecialidad = int.Parse(ddlEspecialidad.SelectedValue);
+                    publicaciones = client.listarPorEspecialidad(idEspecialidad).ToList();
+                }
+                else if (ddlCurso.SelectedIndex > 0)
+                {
+                    int idCurso = int.Parse(ddlCurso.SelectedValue);
+                    publicaciones = client.listarPorCurso(idCurso).ToList();
+                }
+                else
+                {
+                    // Si no hay filtro, cargar todas
+                    publicaciones = client.listarPublicacion().ToList();
+                }
+
+                rptPublicaciones.DataSource = publicaciones;
+                rptPublicaciones.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al cargar publicaciones: " + ex.Message);
+            }
+        }
+
+        protected void ddlFacultad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CargarPublicaciones();
+        }
+
+        protected void ddlEspecialidad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CargarPublicaciones();
+        }
+
+        protected void ddlCurso_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CargarPublicaciones();
+        }
+    }
+
+
+    /*
+    public partial class PaginaInicio : System.Web.UI.Page
+    {
         protected UsuarioWSClient usuarioService;
         protected PublicacionWSClient publicacionService;
         protected FacultadWSClient facultadService;
@@ -140,5 +240,5 @@ namespace QhatuPUCPPresentacion.Inicio
             var usuario = Session["usuario"] as usuario;
             return usuario != null ? usuario.idUsuario : 0;
         }
-    }
+    }*/
 }
